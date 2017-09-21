6b89388139d22c524143ebd57d22e2c5b1c1e55a
Gavin O'Connor <gedmund@gmail.com>
2017-09-21 17:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ if (name === "Susan"){
 else {
   console.log("Expected the name to be Susan.")
 }
-if (height === 70){
-  console.log("The height is correct.")
-}
-else {
-  console.log("Expected other number.")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
